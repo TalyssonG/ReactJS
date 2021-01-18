5faf624daf39ac4584fecfa8afdefff25d511759
TalyssonG <lopestalysson99@gmail.com>
2021-01-18 15:29:38 -0300

    Configurando o babel

---
+++ public/script.js
@@ -0,0 +1,6 @@
+/* 
+- Babel: conveter (transpilar) código do React para um código que o browser entenda
+- Webpack: Pra cada tipo de arquivo(.js .css . png) eu vou converter o código de uma forma diferente
+
+- Loaders: babel-loader, css-loader, image-loader
+*/
