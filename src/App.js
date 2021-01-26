5a74768a9545c366edbe6ffa0c070a4dd70c8060
TalyssonG <lopestalysson99@gmail.com>
2021-01-26 15:18:20 -0300

    Importando o css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+
+import './App.css';
+import backgroundImage from './assets/backgroundImage.jpg';
+
 import Header from './componentes/Header';
 
 /* Skils mais importantes do react
@@ -8,28 +12,28 @@ import Header from './componentes/Header';
 */
 function App() {
 
-    
+
     const [projects, setProjects] = useState(['Developer APP', 'Front end']);
 
 
-    function handleAddProject(){
-       // projects.push(`Novo projeto ${Date.now()}`);
+    function handleAddProject() {
+        // projects.push(`Novo projeto ${Date.now()}`);
 
         //Imutabilidade
         setProjects([...projects, `Novo projeto ${Date.now()}`]);
 
-        console.log(projects);
     }
 
     return (
         <>
-            <Header title="projects" />
+            <Header title="Projects" />
 
+            <img width={100} src={backgroundImage}/>
             <ul>
                 {projects.map(project => <li key={project}>{project}</li>)}
             </ul>
 
-            <button type="button" onClick={ handleAddProject }>Adicionar projeto</button>
+            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
         </>
     );
 }
