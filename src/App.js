1a1d5127544e178715b558b56045cec9cfb85cdf
TalyssonG <lopestalysson99@gmail.com>
2021-01-27 20:29:42 -0300

    Cadastrando Projetos

--- src/App.js
+++ src/App.js
@@ -21,12 +21,17 @@ function App() {
         });
     }, []);
 
-    function handleAddProject() {
+    async function handleAddProject() {
         // projects.push(`Novo projeto ${Date.now()}`);
-
         //Imutabilidade
-        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+       // setProjects([...projects, `Novo projeto ${Date.now()}`]);
+      const response = await api.post('/projects', {
+        title: `Novo projeto ${Date.now()}`,
+        owner: "Talysson Gadêlha"
+    });
+        const project = response.data;
 
+        setProjects([...projects, project])
     }
 
     return (
